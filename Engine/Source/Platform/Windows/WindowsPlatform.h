fe910fbbcb62de5a4132428ba526f157d4275667
文千 崔 <1727146972@qq.com>
2025-07-21 22:38:38 +0800

    <功能修改>(引擎源码平台层):移除一部分对Windows API的禁用

--- Engine/Source/Platform/Windows/WindowsPlatform.h
+++ Engine/Source/Platform/Windows/WindowsPlatform.h
@@ -1,7 +1,7 @@
 #pragma once
-#define WIN32_LEAN_AND_MEAN  // 排除不常用的 API（如 Shell、注册表等）
-#define NOMINMAX            // 禁用 min/max 宏定义（避免与 std::min/max 冲突）
-#include "Windows.h"
+
+
+#define NOMINMAX // 禁用 min/max 宏定义（避免与 std::min/max 冲突）
 
 #include <Windows.h>
 #include "../Platform.h"
