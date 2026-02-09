b65fea93c35da0b2411921071d57c46b354e33db
文千 崔 <1727146972@qq.com>
2026-02-10 01:52:12 +0800

    <新增功能>：Engine 新增 Windows 入口

---
+++ Engine/Source/Launch/Windows/WindowsLaunch.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <Windows.h>
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+    std::cout << "Hello Engine" << std::endl;
+    return 0;
+}
