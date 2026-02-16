01a12066dcc74fa7d35d8bce28a37bf8daf02aa3
文千 崔 <1727146972@qq.com>
2026-02-16 23:49:59 +0800

    <新增功能>：Engine 服务层，核心服务新增全局 GWindowService 全局单例

--- Engine/Source/Service/CoreService.h
+++ Engine/Source/Service/CoreService.h
@@ -2,16 +2,20 @@
 
 #include "Core.h"
 #include "FrameService/FrameService.h"
+#include "WindowService/WindowService.h"
 
 // ================== 全局变量 ==================​​
 inline FrameService* GFrameService = nullptr;
+inline WindowService* GWindowService = nullptr;
 
 inline void AllocCoreServer()
 {
     GFrameService = new FrameService();
+    GWindowService = new WindowService();
 }
 
 inline void FreeCoreServer()
 {
     if (!GFrameService) delete GFrameService;
+    if (!GWindowService) delete GWindowService;
 }
