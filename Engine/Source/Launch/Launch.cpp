6bbbabf2277d438ad9a00da45c74479a54799b6a
文千 崔 <1727146972@qq.com>
2026-02-16 14:53:59 +0800

    <新增功能>：Engine 入口，通用入口新增解析命令行参数判断要启动的应用程序类型逻辑

--- Engine/Source/Launch/Launch.cpp
+++ Engine/Source/Launch/Launch.cpp
@@ -15,8 +15,31 @@ int32 EngineMain(const CommandLineArgs& cmdArgs)
     // 加载核心模块
     PreInitCoreModule();
 
+    // 解析命令行参数判断要启动的应用程序类型
+    Application::Type type = Application::None;
+    if (cmdArgs.HasArg("-type"))
+    {
+        String value = cmdArgs.GetArgValue("-type");
+        if (value == "Game")
+        {
+            type = Application::Game;
+        }
+        if (value == "Editor")
+        {
+            type = Application::Editor;
+        }
+        if (value == "App")
+        {
+            type = Application::App;
+        }
+        if (value == "Server")
+        {
+            type = Application::Server;
+        }
+    }
+
     // 获取应用程序单例
-    Application& app = Application::Create();
+    Application& app = Application::Create(type);
 
     // 应用程序初始化
     int32 error = app.Init();
