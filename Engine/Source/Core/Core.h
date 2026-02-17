d1f27f614fce64ab50a258a88b33395f918235e5
文千 崔 <1727146972@qq.com>
2026-02-17 22:52:52 +0800

    <新增功能>：Engine 核心层，Core.h 新增函数绑定宏

--- Engine/Source/Core/Core.h
+++ Engine/Source/Core/Core.h
@@ -1,3 +1,15 @@
 #pragma once
 
 #include "CoreType.h"
+
+/// <summary>
+/// 回调绑定宏
+/// <para>仅绑定类成员函数</para>
+/// </summary>
+#define MemberFuncBind(func) [this](auto&&... args) -> decltype(auto) { return this->func(std::forward<decltype(args)>(args)...); }
+
+/// <summary>
+/// 回调绑定宏
+/// <para>绑定静态成员函数或普通函数</para>
+/// </summary>
+#define FuncBind(func) [](auto&&... args) -> decltype(auto) { return func(std::forward<decltype(args)>(args)...); }
