5cf252c543b6466715cf48ba76bb07e751a6bee3
文千 崔 <1727146972@qq.com>
2026-02-18 11:41:47 +0800

    <新增功能>：Engine 功能层，核心模块新增 GEventSystem 全局单例

--- Engine/Source/Module/Module.h
+++ Engine/Source/Module/Module.h
@@ -3,12 +3,15 @@
 #include "Core.h"
 #include "Console/Console.h"
 #include "Logger/Logger.h"
+#include "EventSystem/EventSystem.h"
 
 inline Console* GConsole = nullptr;
 inline Logger* GLog = nullptr;
+inline EventSystem* GEventSystem = nullptr;
 
 inline void PreInitCoreModule()
 {
     GConsole = &Console::GetInstance();
     GLog = &Logger::GetInstance();
+    GEventSystem = &EventSystem::GetInstance();
 }
